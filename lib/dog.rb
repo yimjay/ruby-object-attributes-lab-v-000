4ad664de248fd556786f16764ad6cff229f7120a
Ji Yim <gsooyim@gmail.com>
2018-04-09 16:14:44 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,7 @@
 class Dog 
-  def initialize(name)
+  def initialize(name, breed)
     @name = name
+    @breed = breed
   end
   
   def name
@@ -10,4 +11,13 @@ class Dog
   def name=(dog_name)
     @name = dog_name
   end
+  
+  def breed
+    @breed
+  end
+  
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+  
 end
